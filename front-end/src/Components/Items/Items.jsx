d654d4a7b54343d29cb61fc62ea00bb3b92fddb3
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-03-05 11:12:30 +0000

    Added Item styles and fixed syntax errors

--- front-end/src/Components/Items/Items.jsx
+++ front-end/src/Components/Items/Items.jsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import './Items.css';
+import React from 'react'
+import './Items.css'
 
 const Items = (props) => {
     return (
@@ -7,10 +7,10 @@ const Items = (props) => {
             <img src={props.image} alt="" />
             <p>{props.name}</p>
             <div className="item-prices">
-                <div classname="item-price-new">
+                <div className="item-price-new">
                     {props.new_price}
                 </div>
-                <div classname="item-price-old">
+                <div className="item-price-old">
                     {props.old_price}
                 </div>
             </div>
