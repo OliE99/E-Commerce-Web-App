ba92797011b062c1e72bb4aaa2d025ac27ac649a
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-03-05 10:25:51 +0000

    Created items component and styles

--- front-end/src/Components/Items/Items.jsx
+++ front-end/src/Components/Items/Items.jsx
@@ -1,9 +1,19 @@
 import React from 'react';
+import './Items.css';
 
-const Items = () => {
+const Items = (props) => {
     return (
-        <div>
-
+        <div className='item'>
+            <img src={props.image} alt="" />
+            <p>{props.name}</p>
+            <div className="item-prices">
+                <div classname="item-price-new">
+                    {props.new_price}
+                </div>
+                <div classname="item-price-old">
+                    {props.old_price}
+                </div>
+            </div>
         </div>
     )
 }
