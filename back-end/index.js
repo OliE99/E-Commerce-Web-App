4ab660d648c05d59e16972af0dbc6b9bbdec1466
Oliver Elliott <146334137+OliE99@users.noreply.github.com>
2024-03-12 15:28:28 +0000

    Added Login and register endpoints

--- back-end/index.js
+++ back-end/index.js
@@ -125,6 +125,80 @@ app.get('/allproducts', async (req, res) => {
     res.send(products);
 })
 
+// Create Schema for user model
+
+const Users = mongoose.model('Users', {
+    name:{
+        type: String,
+    },
+    email:{
+        type: String,
+        unique: true, 
+    },
+    password: {
+        type: String,
+    },
+    cartData:{
+        type: Object,
+    },
+    date: {
+        type: Date,
+        default: Date.now,
+    }
+})
+
+// Create endpoint for registering Users
+
+app.post('/signup', async (req, res) => {
+    let check = await Users.findOne({email:req.body.email});
+    if(check){
+        return res.status(400).json({success:false, errors:"Exisitng user found with this email id"});
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+    }
+    const user = new Users({
+        name: req.body.username,
+        email: req.body.email,
+        password: req.body.password,
+        cartData: cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user:{
+            id: user.id
+        }
+    }
+
+    const token = jwt.sign(data, 'secret_ecom');
+    res.json({success: true, token});
+});
+
+// Create endpoint for user login
+
+app.post('/login', async (req, res) => {
+    let user = await Users.findOne({email:req.body.email});
+    if (user) {
+        const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user: {
+                    id: user.id
+                }
+            }
+            const token = jwt.sign(data, 'secret_ecom');
+            res.json({sucess: true, token});
+        } else{
+            res.json({success: false, errors:"Wrong Password"});
+        }
+    } else {
+        res.json({success:false, errors:"Wrong Email ID"});
+    }
+});
+
 app.listen(port,(error)=>{
     if(!error) {
         console.log("Server Running on Port " + port)
